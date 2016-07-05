da55e7b77854b07aa69fdd86a1188ed1c3c54a1c
Charlie <charkz102@gmail.com>
2016-07-05 16:05:47 +0800

    First Commit

---
+++ ChapterTwo/ChapterTwoPage.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace ChapterTwo
+{
+	public partial class ChapterTwoPage : ContentPage
+	{
+		
+
+		Label labelSmall = new Label
+		{
+			Text = "Charlie\n" +
+			"Duavis\n" +
+			"Pogoy",
+			FontSize = 20,
+			HorizontalOptions = LayoutOptions.CenterAndExpand
+		};
+
+		Label labelLarge = new Label
+		{
+			Text = "Label",
+			FontSize = 40,
+			HorizontalOptions = LayoutOptions.Center
+		};
+
+
+		Button button = new Button
+		{
+			Text = "Click",
+			FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
+			HorizontalOptions = LayoutOptions.Center,
+			VerticalOptions = LayoutOptions.Fill
+
+
+		};
+
+
+		Entry entry = new Entry
+		{
+			Placeholder = "Username",
+			VerticalOptions = LayoutOptions.Center,
+			Keyboard = Keyboard.Text
+		};
+
+
+		//BoxView boxView = new BoxView
+		//{
+		//	Color = Color.Silver,
+		//	WidthRequest = 150,
+		//	HeightRequest = 150,
+		//	HorizontalOptions = LayoutOptions.StartAndExpand,
+		//	VerticalOptions = LayoutOptions.Fill
+		//};
+
+		//Image image = new Image
+		//{
+		//	Source = "monkey.png",
+		//	Aspect = Aspect.AspectFit,
+		//	HorizontalOptions = LayoutOptions.CenterAndExpand,
+		//	VerticalOptions = LayoutOptions.Fill
+		//};
+
+		//-----------Button----
+		public void idunno(object sender, EventArgs e)
+		{
+			if (button.BackgroundColor == Color.Pink)
+			{
+
+				button.BackgroundColor = Color.Blue;
+				button.Text = "Pink";
+
+				labelSmall.TextColor = Color.Green;
+				labelLarge.FontSize = 20;
+
+			}
+			else
+			{
+				button.BackgroundColor = Color.Pink;
+				button.Text = "Blue";
+				labelSmall.TextColor = Color.Purple;
+				labelLarge.FontSize = 40;
+			}
+
+			String s = entry.Text;
+
+
+				if (s.Length == 0)
+				{
+					this.BackgroundColor = Color.Red;
+					DisplayAlert("Ambot", "This is Empty P**k", "Fin");
+				}
+				else
+				{
+					this.BackgroundColor = Color.Default;
+					DisplayAlert("Alert", s, "OK");
+				}
+
+
+		}
+
+		//------End Button ------
+
+
+		public ChapterTwoPage()
+		{
+			InitializeComponent();
+
+
+			button.Clicked += idunno;
+
+
+			//var tapGestureRecognizer = new TapGestureRecognizer();
+			//tapGestureRecognizer.Tapped += async (sender, e) =>
+		 //  {
+			//   image.Opacity = .5;
+			//   await Task.Delay(200);
+			//   image.Opacity = 1;
+		 //  };
+			//image.GestureRecognizers.Add(tapGestureRecognizer);
+
+			StackLayout stackLayout = new StackLayout
+			{
+				Children =
+				{
+					labelLarge,
+					labelSmall,
+					button,
+					entry
+				},
+				HeightRequest = 1500
+			};
+
+			ScrollView scrollView = new ScrollView
+			{
+				//BackgroundColor = Color.White,
+		
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				Content = stackLayout
+			};
+
+			//this.BackgroundColor = Color.Black; //White
+
+			// Accomodate iPhone status bar.
+			this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+			this.Content = scrollView;
+		}
+
+}
+}
+
---
+++ ChapterTwo/UIAlertView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ChapterTwo
+{
+	class UIAlertView
+	{
+		object p1;
+		object p2;
+		string v1;
+		string v2;
+		string v3;
+
+		public UIAlertView(string v1, string v2, object p1, string v3, object p2)
+		{
+			this.v1 = v1;
+			this.v2 = v2;
+			this.p1 = p1;
+			this.v3 = v3;
+			this.p2 = p2;
+		}
+
+		internal void Show()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
